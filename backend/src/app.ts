295ec8f0afd05ce82c8416f245c4975648891401
LightningItself <68198395+LightningItself@users.noreply.github.com>
2023-07-10 20:30:51 +0530

    added cors to backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,14 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 dotenv.config();
-import express from 'express'
-import booksRouter from './routes/booksRouter';
-import studentsRouter from './routes/studentsRoutes';
-
-
+import express from "express";
+import booksRouter from "./routes/booksRouter";
+import studentsRouter from "./routes/studentsRoutes";
+import cors from "cors";
 const app: express.Application = express();
 
+app.use(cors());
 app.use(express.json());
-app.use('/api/v1/books/', booksRouter);
-app.use('/api/v1/students/', studentsRouter);
+app.use("/api/v1/books/", booksRouter);
+app.use("/api/v1/students/", studentsRouter);
 
 export default app;
